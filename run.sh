49333bdb4137d93eac89fa82057aa5d09efefe38
JOEL UDU <joel.udu@ext.mercedes-benz.io>
2023-10-14 19:37:47 -0600

    create run checks

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-node server.js
+# Check if a filename was provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+# Assign the filename to a variable
+FILENAME=$1
+
+# Start the server with the specified filename
+node src/server.js "$FILENAME"
